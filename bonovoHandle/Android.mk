3f3ce87336d7737fb0c76bba2a4d3bb7f8aa746a
burmjeff <burmjeff@gmail.com>
2016-08-21 00:22:07 -0400

    Revert "Fix annotations"
    This reverts commit 6f7e9678c6da119cff846bb144013c7674664d94.

--- bonovoHandle/Android.mk
+++ bonovoHandle/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := guava librecyclerviewv7 libcardviewv7 libsupportv4 libannotations
+LOCAL_STATIC_JAVA_LIBRARIES := guava librecyclerviewv7 libcardviewv7 libsupportv4
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -14,7 +14,6 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.cardview
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v4
-LOCAL_AAPT_FLAGS += --extra-packages android.support.annotation
 
 LOCAL_PACKAGE_NAME := BonovoHandle
 LOCAL_CERTIFICATE := platform
@@ -26,9 +25,8 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := librecyclerviewv7:libs/android-support-v7-recyclerview.jar \
-                                        libcardviewv7:libs/android-support-v7-cardview.jar \
-                                        libsupportv4:libs/android-support-v4.jar \
-                                        libannotations:libs/android-support-annotations.jar
+										libcardviewv7:libs/android-support-v7-cardview.jar \
+										libsupportv4:libs/android-support-v4.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
